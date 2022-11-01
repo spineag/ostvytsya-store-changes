fdff270f7cb73a28135a7492f24859b5e4a7be35
Andy Grynkiv <spineag@gmail.com>
2022-11-01 15:38:49 +0200

    update 8

--- catalog/view/javascript/ostvytsya.js
+++ catalog/view/javascript/ostvytsya.js
@@ -3,12 +3,10 @@ function onWindowResize(){
     let w;
     if (wWindow>=1000){
         w = $('.left-side-cell').width();
-        console.log('w left: ' + w);
         $('.left-side').width(w);
         w = $('.header-cell').width();
-        console.log('w header: ' + w);
         $('header').width(w);
     }
 }
 window.onresize = onWindowResize;
-onWindowResize();
+$( document ).ready( onWindowResize );
