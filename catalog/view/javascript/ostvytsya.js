3310521861a25179024e8d395e9dce60e36dbf25
Andy Grynkiv <spineag@gmail.com>
2022-11-10 14:49:45 +0200

    search update 9

--- catalog/view/javascript/ostvytsya.js
+++ catalog/view/javascript/ostvytsya.js
@@ -14,7 +14,12 @@ $( document ).ready( onWindowResize );
 $('.div_like-small-button').click(function(e){
     e.stopPropagation();
     e.preventDefault();
-    $(this).find('a').click();
+    let btn = $(this);
+    if (btn.hasClass('btn-search_ost')){
+        if (btn.hasClass('btn-search_ost').hasClass('search_focus')) 
+            btn.find('a').click();
+        else btn.addClass('search_focus'); 
+    } else btn.find('a').click();
 })
 
 // serach: https://freefrontend.com/css-search-boxes/
