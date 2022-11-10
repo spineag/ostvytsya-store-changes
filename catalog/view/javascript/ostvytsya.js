9a445727e5747f25a52bd35cc83aa721edcc4ba8
Andy Grynkiv <spineag@gmail.com>
2022-11-10 19:52:51 +0200

    search 13

--- catalog/view/javascript/ostvytsya.js
+++ catalog/view/javascript/ostvytsya.js
@@ -11,17 +11,8 @@ function onWindowResize(){
 window.onresize = onWindowResize;
 $( document ).ready( onWindowResize );
 
-$('.div_like-small-button').click(function(e){
-    e.stopPropagation();
-    e.preventDefault();
-    // $(this).find('a').click();
-})
-
-$('.search_top_cont').click(function(e){
-    let b = $(this).find('.btn-search_ost');
-    if (b.hasClass('search_focus')) 
-        $(this).find('search').click();
-    else b.addClass('search_focus')
+$('.search_top_cont').parent().click(function(e){
+    $(this).addClass('search_focus');
 });
 
 // serach: https://freefrontend.com/css-search-boxes/
