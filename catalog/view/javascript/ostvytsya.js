a62fc45bfcf7f11bf208a9b4939956ddeaacc1d8
Andy Grynkiv <spineag@gmail.com>
2022-11-11 14:36:35 +0200

    search update 20

--- catalog/view/javascript/ostvytsya.js
+++ catalog/view/javascript/ostvytsya.js
@@ -11,21 +11,20 @@ function onWindowResize(){
 window.onresize = onWindowResize;0
 $( document ).ready( onWindowResize );
 
+/* SEARCH PART */
 $('.box-search_ost button.btn-search_ost').click(function(e){ // also was commented "Search" part in common.js
     if ($(this).parent().hasClass('search_focus')){
         let url = $('base').attr('href') + 'index.php?route=product/search&language=' + $(this).attr('data-lang');
-        let value = $(this).parent().find('input[name=\'search\']').val();
+        let value = $(this).parent().find('.input-search_ost').val();
         if (value) url += '&search=' + encodeURIComponent(value);
         location = url;
     } else $(this).parent().addClass('search_focus');
 });
 $('.box-search_ost button.x-search_ost').click(function(e){
-    console.log('x');
-    console.log($(this).parent());
-    $(this).parent().find('input[name=\'search\']').val('');
+    $(this).parent().find('.input-search_ost').val('');
     $(this).parent().removeClass('search_focus');
 });
-$('.box-search_ost input[name=\'search\']').on('keypress',function(e) {
+$('.box-search_ost .input-search_ost').on('keypress',function(e) {
     if(e.which == 13){
         let url = $('base').attr('href') + 'index.php?route=product/search&language=' + $(this).parent().find('button.btn-search_ost').attr('data-lang');
         let value = $(this).val();
@@ -34,9 +33,10 @@ $('.box-search_ost input[name=\'search\']').on('keypress',function(e) {
     }
 });
 $(document).keyup(function(e){
-    console.log(e.key);
     if (e.key === "Escape" && $('.box-search_ost').hasClass('search_focus')){
-        $('.box-search_ost input[name=\'search\']').val('');
+        $('.box-search_ost .input-search_ost').val('');
         $('.box-search_ost').removeClass('search_focus');
+        $('.box-search_ost .input-search_ost').blur();
+        $('.box-search_ost button.btn-search_ost').blur();
     }
 });
