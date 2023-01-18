ced3a54dde6d6bdff6727a6e358761aefabdd3a1
Andy Grynkiv <spineag@gmail.com>
2023-01-18 16:33:17 +0200

    extension add uk-ua

---
+++ extension/opencart/catalog/language/uk-ua/payment/cod.php
@@ -0,0 +1,7 @@
+<?php
+// Heading
+$_['heading_title']        = 'Оплата при доставці';
+
+// Error
+$_['error_order_id']       = 'Відстуній order ID в сесії!';
+$_['error_payment_method'] = 'Метод оплати не правильний!';
